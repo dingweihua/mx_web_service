af9e7f9a294e0156f2590e4dec5c1dded8823676
wayne <imwaynekk@gmail.com>
2017-11-16 19:55:02 +0800

    featue "爬虫"

---
+++ spider/spider/pipelines.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymongo
+
+from scrapy.conf import settings
+from scrapy.exceptions import DropItem
+from scrapy import log
+
+class MongoDBPipeline(object):
+
+    def __init__(self):
+        connection = pymongo.MongoClient(
+                settings['MONGODB_SERVER'],
+                settings['MONGODB_PORT']
+                )
+        db = connection[settings['MONGODB_DB']]
+        self.connection = db[settings['MONGODB_COLLECTION']]
+
+    def process_item(self, item, spider):
+        valid = True
+        for data in item:
+            if not data:
+                valid = False
+                raise DropItem("Missing {0}!".format(data))
+        if valid:
+            item = dict(item)
+            self.connection.replace_one({'movie_name':item.get('movie_name',''),'ranking':item.get('ranking','')},item,upsert = True)
+            log.msg("add to db", level=log.DEBUG, spider=spider)
+        return item
