a336f360239dfae41011b721f10b6265a4f6d05e
dingweihua <dingweihuaic@126.com>
2017-11-19 10:55:25 +0800

    环境和python的基本优化

--- index.py
+++ index.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask
-from pymongo import MongoClient
 import mongfun
-from bson.json_util import dumps
+import ujson
 app = Flask(__name__)
 
 @app.route("/get_movie_list")
 def hello():
     collection = mongfun.get_mongo_collection('db_douban', 'movie','')
-    return dumps(collection.find())
+    return ujson.dumps(collection.find())
